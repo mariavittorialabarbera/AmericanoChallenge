7eb187d18dea80726048a1b973dcfcc8497149c4
mariavittorialabarbera <mlabarbera22@fed.idserve.net>
2022-11-17 12:20:53 +0100

    niente

---
+++ AmericanoChallengeNC1/AmericanoChallengeNC1/AmericanoChallengeNC1App.swift
@@ -0,0 +1,17 @@
+//
+//  AmericanoChallengeNC1App.swift
+//  AmericanoChallengeNC1
+//
+//  Created by Mariavittoria La Barbera on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct AmericanoChallengeNC1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
